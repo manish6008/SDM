c0f30b1c3f19f80f96b53070a334b980bbc09c84
manish6008 <manishchaudhari6008@gmail.com>
2023-02-02 14:28:41 +0530

    i insert display function and add it in switch case

--- link.java
+++ link.java
@@ -32,7 +32,15 @@ class SinglyList{
                 trav.next=newNode;
             }
           }
-
+           void display(){
+            System.out.println("List: " );
+            Node trav = head;
+            while(trav.next!=null){
+                System.out.println(trav.data);
+                trav=trav.next;
+            }
+            System.out.println("");
+             }
 
         public class SinglyListMain{
             public static void main(String[] args){
@@ -46,7 +54,7 @@ class SinglyList{
 
                 Scanner sc= new Scanner(System.in);
                 do{
-                    system.out.println("press 1 for add first Node \n press 2 for add element at Last Node \n press 3 for exit");
+                    system.out.println("press 1 for add first Node \n press 2 for add element at Last Node \n press 3 for display \n press 4 for exit");
 
                 choice= sc.nextInt();
                 switch(choice){
@@ -62,9 +70,13 @@ class SinglyList{
                     list.addFirst(val);
                     break;
 
+                    case:3 
+                    list.display();
+                    break;
+
                 }
                 }
-                while(choice!=3);
+                while(choice!=4);
             }
         }
     }
